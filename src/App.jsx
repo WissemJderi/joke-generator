4a13624cdeec7690e5e939c0eaa721d785264338
Wissem Jderi <asqr43641@gmail.com>
2025-02-14 08:59:56 +0100

    initialize React app with Vite, add basic components and styles

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1> Joke Generator Using React and Joke API</h1>
+      <button>Click to generate a joke</button>
+    </>
+  );
+}
+
+export default App;
